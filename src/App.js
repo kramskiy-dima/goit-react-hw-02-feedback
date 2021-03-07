fe980c79c65e67f6cf157bc6072952a9f845f9a5
Kramskiy Dima <64987701+ifinddima@users.noreply.github.com>
2021-03-07 23:41:28 +0200

    adds feedback apps

--- src/App.js
+++ src/App.js
@@ -1,7 +1,67 @@
-import React from "react";
+import React, { Component } from "react";
+import Statistics from "./components/Statistics";
+import FeedbackOptions from "./components/FeedbackOptions";
+import Section from "./components/Section";
+import Notification from "./components/Notification";
 
-function App() {
-  return <div className="App"></div>;
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onLeaveFeedback = (e) => {
+    const feedbackButton = e.target.textContent;
+    this.setState((prevState) => {
+      return { [feedbackButton]: prevState[feedbackButton] + 1 };
+    });
+  };
+
+  countTotalFeedback = () => {
+    const totalValue = Object.values(this.state).reduce((acc, value) => {
+      return acc + value;
+    }, 0);
+
+    return totalValue;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const percentage = Math.ceil(
+      (this.state.good / this.countTotalFeedback()) * 100
+    );
+    return percentage ? percentage : 0;
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const totalValueFeedback = this.countTotalFeedback();
+    const totalValuePercentage = this.countPositiveFeedbackPercentage();
+    const feedBackOptionsArr = Object.keys(this.state);
+    return (
+      <div className="App">
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={feedBackOptionsArr}
+            onLeaveFeedback={this.onLeaveFeedback}
+          />
+        </Section>
+        <Section title="Statistics">
+          {totalValueFeedback === 0 ? (
+            <Notification message="No feedback given" />
+          ) : (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={totalValueFeedback}
+              positivePercentage={totalValuePercentage}
+            />
+          )}
+        </Section>
+      </div>
+    );
+  }
 }
 
 export default App;
